3513cbeb4f19a9ef40bc23b6785d5235c58afb69
Michael Schleichardt <michael@schleichardt.net>
2013-03-09 18:40:15 +0100

    added explicit junit-dep dependency to make it possible to work with SBT

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "junit" % "junit-dep" % "4.11" % "test"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
