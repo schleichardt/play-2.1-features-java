44d28ad17c1656474f8bdc4aa6c4aa2787f9a3ac
Michael Schleichardt <michael@schleichardt.net>
2013-03-10 21:24:38 +0100

    refactoring: code as in slides

--- modules/admin/app/controllers/admin/Application.java
+++ modules/admin/app/controllers/admin/Application.java
@@ -1,17 +1,19 @@
 package controllers.admin;
-
 import controllers.AssetsBuilder;
+import play.api.mvc.AnyContent;
+import play.api.mvc.Action;
 import play.mvc.*;
-import play.mvc.Result;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(views.html.admin.index.render());
     }
+
     private static AssetsBuilder delegate = new AssetsBuilder();
 
-    public static play.api.mvc.Action<play.api.mvc.AnyContent> asset(String path, String file) {
+    public static Action<AnyContent> asset(String path, String file) {
         return delegate.at(path, file);
     }
+
 }
