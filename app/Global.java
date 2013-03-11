6877a8ba55b3103a1d0499fd9680f0e3389fbdd4
Michael Schleichardt <michael@schleichardt.net>
2013-03-11 08:36:38 +0100

    example for onLoadConfig

--- app/Global.java
+++ app/Global.java
@@ -1,9 +1,16 @@
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.impl.ConfigImpl;
 import play.*;
 import play.api.mvc.EssentialFilter;
 import play.mvc.*;
 import static play.mvc.Results.*;
 
 import play.filters.csrf.CSRFFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Global extends GlobalSettings {
     @Override
@@ -14,5 +21,34 @@ public class Global extends GlobalSettings {
     @Override
     public <T extends EssentialFilter> Class<T>[] filters() {
         return  new Class[]{CSRFFilter.class};
+    }
+
+    @Override
+    public Configuration onLoadConfig(Configuration configuration, File file, ClassLoader classLoader) {
+        Configuration resultConf = configuration;
+        if (configuration.getInt("embedmongo.port") == null) {
+            Config config = ConfigFactory.parseString("embedmongo.port=" + getFreePort());
+            //there is a Typesafe config wrapped in Play Scala, wrapped in Play Java
+            //merging configurations
+            resultConf = new Configuration(config.withFallback(configuration.getWrappedConfiguration().underlying()));
+        }
+        return resultConf;
+    }
+
+    private int getFreePort() {
+        ServerSocket socket = null;
+        try {
+            socket = new ServerSocket(0);
+            return socket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(socket != null)
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+        }
     }
 }
